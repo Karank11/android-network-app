6f01cd2b3e06fe9b81e92b56b57be6aab97330c7
karankarhale <karanp.karhale@gmail.com>
2025-01-19 16:55:01 +0530

    use GsonConverterFactory

--- app/src/main/java/com/example/retrofitapp/network/MarsApiService.kt
+++ app/src/main/java/com/example/retrofitapp/network/MarsApiService.kt
@@ -1,21 +1,14 @@
 package com.example.retrofitapp.network
 
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-
 
 private const val BASE_URL = "https://mars.udacity.com/"
 
-private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
-    .build()
-
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addConverterFactory(GsonConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
 
