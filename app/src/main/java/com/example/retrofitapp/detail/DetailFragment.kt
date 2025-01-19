fbc3ad0bdf670c663aa4a5572e14b12b8b90210f
karankarhale <karanp.karhale@gmail.com>
2025-01-19 11:54:37 +0530

    fragments and navigation impl

--- app/src/main/java/com/example/retrofitapp/detail/DetailFragment.kt
+++ app/src/main/java/com/example/retrofitapp/detail/DetailFragment.kt
@@ -5,16 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.retrofitapp.R
-
+import com.example.retrofitapp.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding: FragmentDetailBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
+        binding.lifecycleOwner = this
+
+        return binding.root
     }
 }
---
+++ app/src/main/java/com/example/retrofitapp/detail/DetailViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.retrofitapp.detail
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel: ViewModel() {
+}
