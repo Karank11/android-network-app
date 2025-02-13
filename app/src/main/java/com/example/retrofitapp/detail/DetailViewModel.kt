8561acf43fd912694bd666b2796906bb55fb1348
karankarhale <karanp.karhale@gmail.com>
2025-02-13 10:54:38 +0530

    nav safe args dependency

--- app/src/main/java/com/example/retrofitapp/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/retrofitapp/detail/DetailViewModel.kt
@@ -1,6 +1,16 @@
 package com.example.retrofitapp.detail
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.retrofitapp.network.MarsProperty
 
-class DetailViewModel: ViewModel() {
+class DetailViewModel(marsProperty: MarsProperty, app: Application): AndroidViewModel(app) {
+    private val _selectedProperty = MutableLiveData<MarsProperty>()
+    val selectedProperty: LiveData<MarsProperty> get() = _selectedProperty
+
+    init {
+        _selectedProperty.value = marsProperty
+    }
 }
